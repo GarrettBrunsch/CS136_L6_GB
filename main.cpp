9990a97bfb0ade5da0b5198570a1aed4bd94de5d
GarrettBrunsch <brunscgv1595@student.laccd.edu>
2025-04-11 00:39:37 -0700

    Update main with requested changes
    •Remove extra try/catch in CompareQueueContents & Enqueue
    •Improve node handling when temp exits scope in assignment operator (-10)
    •Create empty objects upon constructor failure and add catch for bad_alloc in CopyNodesFrom()
    •CopyNodesFrom() now properly clears nodes upon failure (-20)
    •Rework logic to remove instances of "this->" (-2)
    •Removed function definitions above main (-10)
    •Add missing constructor for Node() (-5)
    •Reorganize code based on suggested style guidelines

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 // Garrett, Brunsch
 // Lab #6 Queue File Processing S25
 // CS_136_14728SP25P
-// Due 3/30/25
+// Due 3/30/25 with resubmission due 4/13
 
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <string>
 #include <limits>
+#include <stdexcept>
+#include <utility>
 
 using namespace std;
 
@@ -20,7 +22,7 @@ const string DIFF_ALL_CASE = "in_diff_all.txt";
 const string EMPTY_FILE_CASE = "in_empty.txt";
 
 const string BASE_CASE = "in_base_case.txt"; // Default for File 1
-const string COMPARISON_CASE = IDENTICAL_CASE; // Default for File 2. Second default is only relevant for manual testing
+const string COMPARISON_CASE = IDENTICAL_CASE; // Default for File 2
 
 enum class MenuOption
 {
@@ -38,103 +40,195 @@ enum class CharType
     COUNT
 };
 
-//Struct used exclusively to assist with automated test cases. 
-// This is my first time playing with more automated testing so I'd appreciate feedback on if this is over-engineering 
-struct TestCase 
+struct TestCase
 {
     string baseFile = "n/a";
     string testFile = "n/a";
     string testName = "n/a";
 };
 
-class Node;
-class Queue;
-class FileManager;
-
 class Node
 {
-public:
-    Node(char data);
-
-    char GetData() const { return data;}
-    Node* GetNext() const { return next;}
-    void SetNext(Node* next);
-
 private:
     char data = '\0';
     Node* next = nullptr;
+
+public:
+    Node();
+    Node(char nodeData);
+
+    char GetData() const;
+    Node* GetNext() const;
+
+    void SetNext(Node* next);
 };
 
 class Queue
 {
+private:
+    Node* front = nullptr;
+    Node* rear = nullptr;
+    int size;
+
+    void ClearNodes();
+    bool CopyNodesFrom(const Queue& other);
+
 public:
     Queue();
     Queue(const Queue& other);
-    Queue& operator=(const Queue& other);
     ~Queue();
+
+    Queue& operator=(const Queue& other);
 
     void Enqueue(char data);
     char Dequeue();
 
-    bool IsEmpty() const { return front == nullptr; }
-    int GetSize() const { return size; }
-
-private:
-    void ClearNodes();
-    bool CopyNodesFrom(const Queue& other);
-
-    Node* front = nullptr;
-    Node* rear = nullptr;
-    int size = 0;
+    bool IsEmpty() const;
+    int GetSize() const;
 };
 
 class FileManager
 {
-public:
-    FileManager();
-    bool CompareFiles(const string& fileName1, const string& fileName2);
-    void DisplayStats(const string& fileName1 = BASE_CASE, const string& fileName2 = COMPARISON_CASE);
-    bool IsProcessed() const;
-
 private:
-    bool LoadFileToQueue(const string& fileName, Queue& queue);
-    bool CompareQueueContents(Queue& queue1, Queue& queue2);
-
-    void CountCharTypes(const string& fileName, int stats[static_cast<int>(CharType::COUNT)]);
-
     bool filesProcessed = false;
+    static const int TOTAL_INDEX = static_cast<int>(CharType::COUNT);
     int file1Stats[static_cast<int>(CharType::COUNT) + 1];
     int file2Stats[static_cast<int>(CharType::COUNT) + 1];
 
-    static const int TOTAL_INDEX = static_cast<int>(CharType::COUNT);
+    bool LoadFileToQueue(const string& fileName, Queue& queue);
+    bool CompareQueueContents(Queue& queue1, Queue& queue2);
+    void CountCharTypes(const string& fileName, int stats[static_cast<int>(CharType::COUNT) + 1]);
+
+public:
+    FileManager();
+
+    bool CompareFiles(const string& fileName1, const string& fileName2);
+    void DisplayStats(const string& fileName1, const string& fileName2);
+
+    bool IsProcessed() const;
 };
 
 void DisplayMenu();
-bool ProcessCompareFiles(FileManager& fileManager, const string& fileName1 = BASE_CASE, const string& fileName2 = COMPARISON_CASE);
+bool ProcessCompareFiles(FileManager& fileManager, const string& fileName1, const string& fileName2);
 void RunAutomatedTests(FileManager& fileManager);
 void ClearInputError();
 
-Node::Node(char data) : data(data), next(nullptr)
+int main()
 {
+    FileManager fileManager;
+    int choiceInt = 0;
+
+    do
+    {
+        DisplayMenu();
+        cin >> choiceInt;
+
+        if (cin.fail())
+        {
+            ClearInputError();
+        }
+        else
+        {
+            try
+            {
+                MenuOption option = static_cast<MenuOption>(choiceInt);
+
+                switch (option)
+                {
+                case MenuOption::COMPARE_FILES:
+                    ProcessCompareFiles(fileManager, BASE_CASE, COMPARISON_CASE);
+                    break;
+                case MenuOption::DISPLAY_STATS:
+                    fileManager.DisplayStats(BASE_CASE, COMPARISON_CASE);
+                    break;
+                case MenuOption::RUN_AUTOMATED_TESTS:
+                    RunAutomatedTests(fileManager);
+                    break;
+                case MenuOption::QUIT:
+                    cout << "\nNow exiting program...\n\n";
+                    break;
+                default:
+                    cout << "Invalid choice. Please select a valid menu option";
+                    break;
+                }
+            }
+            catch (const std::exception& e)
+            {
+                cerr << "An error occurred: " << e.what() << "\n";
+            }
+        }
+    } while (choiceInt != static_cast<int>(MenuOption::QUIT));
+
+    return 0;
 }
 
-void Node::SetNext(Node* next)
+Node::Node()
 {
-    this->next = next;
+    data = '\0';
+    next = nullptr;
 }
 
-Queue::Queue() : front(nullptr), rear(nullptr), size(0)
+Node::Node(char nodeData)
 {
+    data = nodeData;
+    next = nullptr;
+}
+
+char Node::GetData() const
+{
+    return data;
+}
+
+Node* Node::GetNext() const
+{
+    return next;
+}
+
+void Node::SetNext(Node* nextNode)
+{
+    next = nextNode;
+}
+
+Queue::Queue()
+{
+    front = nullptr;
+    rear = nullptr;
+    size = 0;
+}
+
+Queue::Queue(const Queue& other)
+{
+    front = nullptr;
+    rear = nullptr;
+    size = 0;
+
+    CopyNodesFrom(other);
+}
+
+Queue::~Queue()
+{
+    ClearNodes();
+}
+
+Queue& Queue::operator=(const Queue& other)
+{
+    if (this != &other)
+    {
+        Queue temp(other);
+        swap(front, temp.front);
+        swap(rear, temp.rear);
+        swap(size, temp.size);
+    }
+    return *this;
 }
 
 void Queue::ClearNodes()
 {
-    Node* current = front;
-    while (current != nullptr)
+    while (front != nullptr)
     {
-        Node* nextNode = current->GetNext();
-        delete current;
-        current = nextNode;
+        Node* temp = front;
+        front = front->GetNext();
+        delete temp;
     }
 
     front = nullptr;
@@ -154,93 +248,31 @@ bool Queue::CopyNodesFrom(const Queue& other)
             Enqueue(current->GetData());
             current = current->GetNext();
         }
-        catch (const bad_alloc& e)
+        catch (const bad_alloc&)
         {
+            ClearNodes();
             success = false;
         }
     }
     return success;
 }
 
-Queue::Queue(const Queue& other) : front(nullptr), rear(nullptr), size(0)
-{
-    bool success = CopyNodesFrom(other);
-
-    if (!success)
-    {
-        ClearNodes();
-        throw bad_alloc();
-    }
-}
-
-Queue& Queue::operator=(const Queue& other)
-{
-    bool success = true;
-
-    if (this != &other)
-    {
-        Queue temp;
-        success = temp.CopyNodesFrom(other);
-
-        if (success)
-        {
-            ClearNodes();
-
-            front = temp.front;
-            rear = temp.rear;
-            size = temp.size;
-
-            temp.front = nullptr;
-            temp.rear = nullptr;
-            temp.size = 0;
-        }
-        else
-        {
-            throw bad_alloc();
-        }
-    }
-    return *this;
-}
-
-Queue::~Queue()
-{
-    ClearNodes();
-}
-
 void Queue::Enqueue(char data)
 {
-    Node* newNode = nullptr;
-    bool success = false;
+    Node* newNode = new Node(data);
 
-    try
+    if (IsEmpty())
     {
-        newNode = new Node(data);
-        success = true;
-    }
-    catch (const bad_alloc& e)
-    {
-        success = false;
-    }
-
-    if (success)
-    {
-        if (IsEmpty())
-        {
-            front = newNode;
-            rear = newNode;
-        }
-        else
-        {
-            rear->SetNext(newNode);
-            rear = newNode;
-        }
-
-        size++;
+        front = newNode;
+        rear = newNode;
     }
     else
     {
-        throw bad_alloc();
+        rear->SetNext(newNode);
+        rear = newNode;
     }
+
+    size++;
 }
 
 char Queue::Dequeue()
@@ -251,28 +283,42 @@ char Queue::Dequeue()
     {
         throw runtime_error("Cannot dequeue from empty queue");
     }
-    else
+
+    Node* temp = front;
+    result = temp->GetData();
+    front = front->GetNext();
+
+    if (front == nullptr)
     {
-        Node* temp = front;
-        result = temp->GetData();
-        front = front->GetNext();
-
-        if (front == nullptr)
-        {
-            rear = nullptr;
-        }
-
-        delete temp;
-        size--;
+        rear = nullptr;
     }
+
+    delete temp;
+    size--;
+
     return result;
 }
 
-FileManager::FileManager(): filesProcessed(false),file1Stats{0}, file2Stats{0}
+bool Queue::IsEmpty() const
 {
+    return front == nullptr;
 }
 
+int Queue::GetSize() const
+{
+    return size;
+}
 
+FileManager::FileManager()
+{
+    filesProcessed = false;
+
+    for (int i = 0; i <= static_cast<int>(CharType::COUNT); i++)
+    {
+        file1Stats[i] = 0;
+        file2Stats[i] = 0;
+    }
+}
 
 bool FileManager::LoadFileToQueue(const string& fileName, Queue& queue)
 {
@@ -290,9 +336,14 @@ bool FileManager::LoadFileToQueue(const string& fileName, Queue& queue)
             {
                 queue.Enqueue(ch);
             }
-            catch (const exception& e)
+            catch (const bad_alloc& e)
             {
                 cerr << "Memory allocation failed:" << e.what() << "\n";
+                continueReading = false;
+            }
+            catch (const exception& e)
+            {
+                cerr << "Error during enqueue:" << e.what() << "\n";
                 continueReading = false;
             }
         }
@@ -309,63 +360,57 @@ bool FileManager::LoadFileToQueue(const string& fileName, Queue& queue)
 
 bool FileManager::CompareQueueContents(Queue& queue1, Queue& queue2)
 {
-    bool areIdentical = false;
+    bool contentMatchStatus = false;
 
     if (queue1.GetSize() == queue2.GetSize())
     {
         int queueSize = queue1.GetSize();
-        bool contentMatches = true;
-        bool exceptionOccurred = false;
+        contentMatchStatus = true;
         int i = 0;
 
-        while (i < queueSize && contentMatches && !exceptionOccurred)
-        {
-            char char1 = '\0';
-            char char2 = '\0';
-
-            try
+            while (i < queueSize && contentMatchStatus)
             {
-                char1 = queue1.Dequeue();
-                char2 = queue2.Dequeue();
-                contentMatches = char1 == char2;
-            }
-            catch (const exception& e)
-            {
-                cerr << "Error when comparing characters " << e.what() << "\n";
-                exceptionOccurred = true;
-            }
+                char char1 = '\0';
+                char char2 = '\0';
 
-            i++;
+                if (!queue1.IsEmpty() && !queue2.IsEmpty())
+                {
+                    char1 = queue1.Dequeue();
+                    char2 = queue2.Dequeue();
+                    contentMatchStatus = char1 == char2;
+                }
+
+                else
+                {
+                    contentMatchStatus = false;
+                }
+
+                i++;
+            }
         }
-
-        areIdentical = contentMatches && !exceptionOccurred;
-    }
-    return areIdentical;
+    return contentMatchStatus;
 }
 
 bool FileManager::CompareFiles(const string& fileName1, const string& fileName2)
 {
     Queue queue1;
     Queue queue2;
-    bool areIdentical = false;
-    bool file1Loaded = false;
-    bool file2Loaded = false;
 
-    file1Loaded = LoadFileToQueue(fileName1, queue1);
-    file2Loaded = LoadFileToQueue(fileName2, queue2);
+    bool file1Loaded = LoadFileToQueue(fileName1, queue1);
+    bool file2Loaded = LoadFileToQueue(fileName2, queue2);
 
     if (file1Loaded && file2Loaded)
     {
         CountCharTypes(fileName1, file1Stats);
         CountCharTypes(fileName2, file2Stats);
         filesProcessed = true;
-
-        areIdentical = CompareQueueContents(queue1, queue2);
     }
+    bool areIdentical = CompareQueueContents(queue1, queue2);
+
     return areIdentical;
 }
 
-void FileManager::CountCharTypes(const string& fileName, int stats[static_cast<int>(CharType::COUNT)])
+void FileManager::CountCharTypes(const string& fileName, int stats[static_cast<int>(CharType::COUNT) +1])
 {
     for (int i = 0; i <= static_cast<int>(CharType::COUNT); i++)
     {
@@ -411,7 +456,7 @@ void FileManager::DisplayStats(const string& fileName1, const string& fileName2)
     }
     else
     {
-        // Headers
+
         cout << left << setw(20) << "File Name"
             << right << setw(12) << "Digits"
             << right << setw(12) << "Letters"
@@ -438,56 +483,14 @@ bool FileManager::IsProcessed() const
     return filesProcessed;
 }
 
-int main()
-{
-    FileManager fileManager;
-    int choiceInt = 0;
-
-    do
-    {
-        DisplayMenu();
-        cin >> choiceInt;
-
-        if (cin.fail())
-        {
-            ClearInputError();
-        }
-        else
-        {
-            MenuOption option = static_cast<MenuOption>(choiceInt);
-
-            switch (option)
-            {
-            case MenuOption::COMPARE_FILES:
-                ProcessCompareFiles(fileManager);
-                break;
-            case MenuOption::DISPLAY_STATS:
-                fileManager.DisplayStats();
-                break;
-            case MenuOption::RUN_AUTOMATED_TESTS:
-                RunAutomatedTests(fileManager);
-                break;
-            case MenuOption::QUIT:
-                cout << "\nNow exiting program...\n\n";
-                break;
-            default:
-                cout << "Invalid choice. Please select a valid menu option";
-                break;
-            }
-        }
-    } while (choiceInt != static_cast<int>(MenuOption::QUIT));
-
-    return 0;
-}
-
 void DisplayMenu()
 {
     cout << "\n\n == Main Menu ==\n"
-    "1. Compare Files\n"
-    "2. Display File Statistics\n"
-    "3. Run Automated Tests\n"
-    "4. Quit\n"
-    "Choice: ";
+        << "1. Compare Files\n"
+        << "2. Display File Statistics\n"
+        << "3. Run Automated Tests\n"
+        << "4. Quit\n"
+        << "Choice: ";
 }
 
 bool ProcessCompareFiles(FileManager& fileManager, const string& fileName1, const string& fileName2)
@@ -509,7 +512,7 @@ bool ProcessCompareFiles(FileManager& fileManager, const string& fileName1, cons
 
 void RunAutomatedTests(FileManager& fileManager)
 {
-    const TestCase testCases[] = 
+    const TestCase testCases[] =
     {
         {BASE_CASE, IDENTICAL_CASE, "Identical Files"},
         {BASE_CASE, DIFF_DIGITS_CASE, "Different Digits"},
@@ -521,7 +524,7 @@ void RunAutomatedTests(FileManager& fileManager)
 
     const int testCount = sizeof(testCases) / sizeof(testCases[0]);
 
-    for (int i = 0; i < testCount; i++) 
+    for (int i = 0; i < testCount; i++)
     {
         const TestCase& test = testCases[i];
 
@@ -537,138 +540,3 @@ void ClearInputError()
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cout << "Invalid choice. Please enter a valid number: ";
 }
-
-/*
-
-
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: g
-Invalid choice. Please enter a valid number:
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: 2
-Files must be compared before displaying the values
-
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: 1
-
-Comparing files in_base_case.txt and in_identical.txt
-Files are identical
-
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: 2
-File Name                 Digits     Letters     Special       Total
---------------------------------------------------------------------
-in_base_case.txt               6          56          31          93
-in_identical.txt               6          56          31          93
-
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: 3
-
-Comparing files in_base_case.txt and in_identical.txt
-Files are identical
-File Name                 Digits     Letters     Special       Total
---------------------------------------------------------------------
-in_base_case.txt               6          56          31          93
-in_identical.txt               6          56          31          93
-
-
-Comparing files in_base_case.txt and in_diff_digits.txt
-Files are different
-File Name                 Digits     Letters     Special       Total
---------------------------------------------------------------------
-in_base_case.txt               6          56          31          93
-in_diff_digits.txt            10          56          32          98
-
-
-Comparing files in_base_case.txt and in_diff_letters.txt
-Files are different
-File Name                 Digits     Letters     Special       Total
---------------------------------------------------------------------
-in_base_case.txt               6          56          31          93
-in_diff_letters.txt            6          86          36         128
-
-
-Comparing files in_base_case.txt and in_diff_special.txt
-Files are different
-File Name                 Digits     Letters     Special       Total
---------------------------------------------------------------------
-in_base_case.txt               6          56          31          93
-in_diff_special.txt            6          56          37          99
-
-
-Comparing files in_base_case.txt and in_diff_all.txt
-Files are different
-File Name                 Digits     Letters     Special       Total
---------------------------------------------------------------------
-in_base_case.txt               6          56          31          93
-in_diff_all.txt               10          92          44         146
-
-
-Comparing files in_base_case.txt and in_empty.txt
-Files are different
-File Name                 Digits     Letters     Special       Total
---------------------------------------------------------------------
-in_base_case.txt               6          56          31          93
-in_empty.txt                   0           0           0           0
-
-
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: 5
-Invalid choice. Please select a valid menu option
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: 4
-
-Now exiting program...
-
-Missing File example:
-
-
- == Main Menu ==
-1. Compare Files
-2. Display File Statistics
-3. Run Automated Tests
-4. Quit
-Choice: 1
-
-Comparing files missing_file_example.txt and missing_identical_example
-Error Could not open file missing_file_example.txt
-Error Could not open file missing_identical_example
-Files are different
-
-
-*/
